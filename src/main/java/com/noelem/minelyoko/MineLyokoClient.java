fab47e2abf291083dd4bcb09d0970246e8228b47
Noel <leonardoulino2005@gmail.com>
2023-07-08 13:47:02 +0200

    Log Action

--- src/main/java/com/noelem/minelyoko/MineLyokoClient.java
+++ src/main/java/com/noelem/minelyoko/MineLyokoClient.java
@@ -6,6 +6,7 @@ public class MineLyokoClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        System.out.println("Mod Client initialized");
+        System.out.println("Running MineLyoko");
     }
 }
