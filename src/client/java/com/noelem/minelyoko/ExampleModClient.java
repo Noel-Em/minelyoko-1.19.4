09b6764d60b7a1de62c8f9dfc5327e8a6d748826
Noel <leonardoulino2005@gmail.com>
2023-07-08 13:45:09 +0200

    Initial Commit

---
+++ src/client/java/com/noelem/minelyoko/ExampleModClient.java
@@ -0,0 +1,10 @@
+package com.noelem.minelyoko;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/noelem/minelyoko/MineLyokoClient.java
@@ -0,0 +1,11 @@
+package com.noelem.minelyoko;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineLyokoClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
